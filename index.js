3a5efb5c8f89f805676acce86519481f8fcd4697
Ali Moradzade <alimorizz1379@gmail.com>
2022-11-17 21:57:29 +0330

    adding a basic ampq cloud api

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-const imagga = require('./apis/imagga');
+// const imagga = require('./apis/imagga');
 // const mailgun = require('./apis/mailgun');
+const cloudamp = require('./apis/ampq');
 
-(async () => {
-    const imageUrl = 'https://wallpapercave.com/wp/wp3503654.jpg';
-    const result = await imagga(imageUrl);
-
-    console.log(result);
-})();
+// (async () => {
+//     const imageUrl = 'https://wallpapercave.com/wp/wp3503654.jpg';
+//     const result = await imagga(imageUrl);
+//
+//     console.log(result);
+// })();
 
 // (async () => {
 //     const email = 'alimorizz1379@gmail.com';
@@ -16,3 +17,5 @@ const imagga = require('./apis/imagga');
 //     await mailgun(email, subject, text);
 // })();
 //
+
+cloudamp();
