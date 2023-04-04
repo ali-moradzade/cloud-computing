6a22d79a110ae75a38491144d44c6223f50694d3
Ali Moradzade <alimorizz1379@gmail.com>
2023-04-04 21:42:57 +0330

    adding basic /upload path

--- service1/src/routes/routes.ts
+++ service1/src/routes/routes.ts
@@ -1,9 +1,29 @@
 import {Request, Response, Router} from 'express';
+import {Form} from "multiparty";
 
 const router = Router();
 
 router.post('/upload', (req: Request, res: Response) => {
     // TODO
+    let form = new Form();
+
+    form.parse(req, async (err, fields: { inputs: string }, files) => {
+        if (err) {
+            console.log(err);
+        } else {
+            let pathOfFile = files.file[0].path;
+            let description = fields.inputs[0];
+
+            console.log('Form Data:');
+            console.log({
+                pathOfFile,
+                description
+            });
+
+
+            res.send('successfully uploaded');
+        }
+    });
 });
 
 export {router};
