adbae6f3c1e224c93e97ffa9145f8491abf68d0a
Ali Moradzade <alimorizz1379@gmail.com>
2023-04-06 19:54:08 +0330

    correcting query params creation

--- service2/src/services/createJob.service.ts
+++ service2/src/services/createJob.service.ts
@@ -16,7 +16,12 @@ export async function createJob(uploadId: string) {
     const content = readFileSync(filePath, 'utf8');
 
     // make a query string from: inputs, language, content
-    const job = `inputs=${inputs}&language=${language}&content=${content}`;
+    const urlencoded = new URLSearchParams();
+    urlencoded.append("code", content);
+    urlencoded.append("language", language);
+    urlencoded.append("input", inputs);
+
+    const job = urlencoded.toString();
 
     // save the job to the database
     const newJob = new Job({
