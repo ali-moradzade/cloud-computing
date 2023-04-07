b91b0fc624a4e346b3dcc9b16a31a31571a0fbd9
Ali Moradzade <alimorizz1379@gmail.com>
2023-04-07 11:39:23 +0330

    using closure to cache connection

--- service1/src/apis/ampq.ts
+++ service1/src/apis/ampq.ts
@@ -1,8 +1,22 @@
 import client from 'amqplib'
 
+/**
+ * Use closure to cache the connection
+ */
+const connection = (function () {
+    let conn: client.Connection;
+
+    return async function () {
+        if (!conn) {
+            conn = await client.connect(process.env.CLOUDAMQP_URL || 'amqp://localhost');
+        }
+        return conn;
+    }
+})();
+
 export async function publish(uploadId: string) {
     const queue: string = process.env.QUEUE_NAME || 'queue';
-    const conn = await client.connect(process.env.CLOUDAMQP_URL || 'amqp://localhost');
+    const conn = await connection();
 
     const ch2 = await conn.createChannel();
     await ch2.assertQueue(queue);
