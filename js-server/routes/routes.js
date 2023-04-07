8e5fe466ee90f63a0029970da016ee639386c702
Ali Moradzade <alimorizz1379@gmail.com>
2023-04-07 12:44:40 +0330

    removing js-server, it was for prev term

--- js-server/routes/routes.js
+++
@@ -1,6 +0,0 @@
-const AdvertisementController = require('../controllers/advertisement.controller');
-
-module.exports = (app) => {
-    app.post('/register', AdvertisementController.createAd);
-    app.get('/ad/:id', AdvertisementController.getAd);
-}
