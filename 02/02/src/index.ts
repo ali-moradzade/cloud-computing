0b65118f0f5f3e4f16fed0c774601a393c88d070
Ali Moradzade <alimorizz1379@gmail.com>
2023-05-10 17:22:47 +0330

    adding client code to connect to redis

--- 02/02/src/index.ts
+++ 02/02/src/index.ts
@@ -1,15 +1,27 @@
 import * as dotenv from 'dotenv';
+import {createClient} from 'redis';
 import express from 'express';
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
+const key = 'hello';
 
-app.get('/', (req: any, res: any) => {
-   res.send('Hello World!');
+const client = createClient({
+    socket: {
+        host: process.env.REDIS_HOST,
+        port: parseInt(process.env.REDIS_PORT || '6379')
+    }
+});
+
+client.on('error', (err) => console.error(err));
+client.connect();
+
+app.get('/', async (req: any, res: any) => {
+    res.send(await client.get(key));
 });
 
 app.listen(port, () => {
-   console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on port ${port}`);
 });
