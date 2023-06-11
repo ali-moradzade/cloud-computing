a64794ac0e7036ba8939c2ff5f0e6e60e9620401
Ali Moradzade <alimorizz1379@gmail.com>
2023-06-11 19:44:12 +0330

    refactoring mapper

--- 03/phase01/mapper.py
+++ 03/phase01/mapper.py
@@ -1,31 +1,30 @@
 import sys
 import csv
 
-def print_formatted(message, likes, retweets, web_app_source, iphone_source, android_source):
-        print(f"{message}\t{likes},{retweets},{web_app_source},{iphone_source},{android_source}")
 
-reader = csv.reader(sys.stdin)
+def print_formatted(message, likes, retweet_count, web_app_source, iphone_source, android_source):
+    print(f"{message}\t{likes},{retweet_count},{web_app_source},{iphone_source},{android_source}")
 
-for i,row in enumerate(reader):
-    if i >= 9:
-        break
 
-    tweet=row[2].lower()
-    likes=row[3]
-    retweet_count=row[4]
-    source=row[5].lower()
+def main():
+    reader = csv.reader(sys.stdin)
+    for row in reader:
+        tweet = row[2].lower()
+        likes = row[3]
+        retweet_count = row[4]
+        source = row[5].lower()
 
-    message=''
+        message = ''
 
-    if "biden" in tweet and "trump" in tweet:
-        message = "Both Candidate"
-    elif "biden" in tweet:
-        message = "Joe Biden"
-    elif "trump" in tweet:
-        message = "Donald Trump"
+        if "biden" in tweet and "trump" in tweet:
+            message = "Both Candidate"
+        elif "biden" in tweet:
+            message = "Joe Biden"
+        elif "trump" in tweet:
+            message = "Donald Trump"
 
-    if message != '':
-        print_formatted(message, likes, retweet_count, web_app_source=int("web app" in source),
-            iphone_source=int("iphone" in source), android_source=int("android" in source),
-        )
+        if message:
+            print_formatted(message, likes, retweet_count, "web app" in source, "iphone" in source, "android" in source)
 
+
+main()
