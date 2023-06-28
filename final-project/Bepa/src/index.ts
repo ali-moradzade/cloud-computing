d4231092bad415b65f39862faba29d47b1f60afb
Ali Moradzade <alimorizz1379@gmail.com>
2023-06-28 14:22:20 +0330

    adding index

--- final-project/Bepa/src/index.ts
+++ final-project/Bepa/src/index.ts
@@ -1,1 +1,11 @@
-console.log('hello world')
+import {connect} from "mongoose";
+import {BEPA} from "./config";
+import {writePricesService} from "./services/writePrices.service";
+import {alertSubscribersService} from "./services/alertSubscribers.service";
+
+(async () => {
+    await connect(BEPA.mongodb.url);
+   
+    await writePricesService();
+    await alertSubscribersService();
+})();
