15f95521b1c0fef9ea05f9a63be27820d626c392
Ali Moradzade <alimorizz1379@gmail.com>
2023-06-30 00:23:41 +0330

    adding some logs, disconnecting from db after doing works

--- final-project/Bepa/src/index.ts
+++ final-project/Bepa/src/index.ts
@@ -1,11 +1,18 @@
-import {connect} from "mongoose";
+import {connect, disconnect} from "mongoose";
 import {BEPA} from "./config";
 import {writePricesService} from "./services/writePrices.service";
 import {alertSubscribersService} from "./services/alertSubscribers.service";
 
 (async () => {
     await connect(BEPA.mongodb.url);
+    console.log('Connected to database ..')
 
     await writePricesService();
+    console.log('latest prices written to db ..')
+
     await alertSubscribersService();
+    console.log('Subscribers alerted ..')
+
+    await disconnect();
+    console.log('Disconnected from db')
 })();
